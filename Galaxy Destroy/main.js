53f2c9b9618782f740aa7ac37509328cf9411b99
PollyanaAGW <pollyanadias91@gmail.com>
2024-09-23 16:19:03 -0300

    adicionado a luz ambiente

--- Galaxy Destroy/main.js
+++ Galaxy Destroy/main.js
@@ -7,10 +7,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+/*const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+scene.add(cube);*/
+
+const light = new THREE.Ambientligth( 0xffffff);
+scene.add(light);
 
 camera.position.z = 5;
 
