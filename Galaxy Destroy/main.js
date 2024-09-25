18cd9999cc87a3f789050fec898c15a54521c8ea
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2024-09-25 08:52:58 -0300

    Colocando os modelos e tentando importar

--- Galaxy Destroy/main.js
+++ Galaxy Destroy/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -7,42 +8,64 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-/*const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);*/
-
-// const light = new THREE.Ambientligth( 0xffffff);
-// scene.add(light);
-
 let model = null;
 
 class NaveEspacial{
 	constructor (){
 		this.model = null;
-		this.load(this);
+		this.load();
 	}
 
 	move(){
-		if(this.model){
-			this.model.position.Z += 0.01;
 
-			if(this.model.position.z >= 5){
-				this.model.position.z = -20.0;
-				let r = Math.random();
-				this.model.position.x = -15.0 * r + 15.0 * (1.0 - r);
-				this.model.position.y = -15.0 * r + 15.0 * (1.0 - r);
-			}
-		}
 	}
+	load(object){
+
+		const loader = new GLTFLoader();
+
+		loader.load(
+			// resource URL
+			'models/Spaceship.glb',
+			// called when the resource is loaded
+			function ( gltf ) {
+				
+				this.model = gltf.scene;
+                scene.add(this.model);
+
+				// scene.add( gltf.scene );
+				// object.model = gltf.scene.children[0];
+		
+				gltf.animations; // Array<THREE.AnimationClip>
+				gltf.scene; // THREE.Group
+				gltf.scenes; // Array<THREE.Group>
+				gltf.cameras; // Array<THREE.Camera>
+				gltf.asset; // Object
+		
+			},
+			// called while loading is progressing
+			function ( xhr ) {
+		
+				console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		
+			},
+			// called when loading has errors
+			function ( error ) {
+		
+				console.log( 'An error happened' );
+		
+			}
+		);
 }
+}
+
+let nave = new NaveEspacial;
+scene.add(nave)
 
 camera.position.z = 5;
 
 function animate() {
+	nave.load();
 	renderer.render(scene, camera);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-}
-renderer.setAnimationLoop(animate);
 
+renderer.setAnimationLoop(animate);
+}
