518ca79b66f788018773c77640aefa559ab00181
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2024-09-25 16:59:18 -0300

    agora roda, mas feio

--- Galaxy Destroy/main.js
+++ Galaxy Destroy/main.js
@@ -13,20 +13,24 @@ let model = null;
 class NaveEspacial{
 	constructor (){
 		this.model = null;
-		this.load();
+		this.load(this);
 	}
 
 	move(){
-		if(this.model){
-			this.model.position.z += 0.01;
+			// this.model.position.z += 0.03; // Adjust the speed here
+			this.model.rotation.x += 0.01;	
+			this.model.rotation.y += 0.02;
+		
+		// if(this.model){
+		// 	this.model.position.z += 0.01;
 
-			if(this.model.position.z >= 5){
-				this.model.position.z = -20.0;
-				let r = Math.random();
-				this.model.position.x = -15.0 *  r + 15.0 * (1.0 - r);
-				this.model.position.y = -15.0 *  r + 15.0 * (1.0 - r);
-			}
-		}
+		// 	if(this.model.position.z >= 5){
+		// 		this.model.position.z = -20.0;
+		// 		let r = Math.random();
+		// 		this.model.position.x = -15.0 *  r + 15.0 * (1.0 - r);
+		// 		this.model.position.y = -15.0 *  r + 15.0 * (1.0 - r);
+		// 	}
+		// }
 	}
 	load(object){
 
@@ -34,22 +38,27 @@ class NaveEspacial{
 
 		loader.load(
 			// resource URL
-			'models/Spaceship.glb',
+			'models/Spaceship.gltf',
 			// called when the resource is loaded
-			function ( glb ) {
+			function ( gltf ) {
 				
-				scene.add( glb.scene );
-                this.model = glb.scene;
+				
+				scene.add( gltf.scene );
+				object.model = gltf.scene.children[0];
+
+				// model = glb.scene.children[0];
+				// console.log(model);
+				// scene.add( model );
+				// model = glb.scene.children[12];
 				// this.scene.add = (this.model)
 
 				// scene.add( gltf.scene );
-				// object.model = gltf.scene.children[0];
 		
-				glb.animations; // Array<THREE.AnimationClip>
-				glb.scene; // THREE.Group
-				glb.scenes; // Array<THREE.Group>
-				glb.cameras; // Array<THREE.Camera>
-				glb.asset; // Object
+				gltf.animations; // Array<THREE.AnimationClip>
+				gltf.scene; // THREE.Group
+				gltf.scenes; // Array<THREE.Group>
+				gltf.cameras; // Array<THREE.Camera>
+				gltf.asset; // Object
 		
 			},
 			// called while loading is progressing
@@ -73,22 +82,30 @@ class NaveEspacial{
 
 let nave = new NaveEspacial();
 
-let light = new THREE.AmbientLight(0xffffff, 10);
+let light = new THREE.AmbientLight(0xff00ff);
 scene.add(light);
 
 camera.position.z = 10;
 
 function animate() {
-
 	renderer.render(scene, camera);
 
-	if (nave.model != null){
-		asteroid.model.rotation.x += 0.05;
-		asteroid.model.rotation.y += 0.03;
-		asteroid.model.rotation.z -= 0.1;
-	}
+	// if (nave.model != null){
+	// 	asteroid.model.rotation.x += 0.05;
+	// 	asteroid.model.rotation.y += 0.03;
+	// 	asteroid.model.rotation.z -= 0.1;
+	// }
 
-	nave.move();
+	if (nave.model) {
+        // Movimento mais visível e rotação suave
+       // nave.model.position.z += 0.1;
+        nave.model.rotation.y += 0.02;
+
+        // Criando um movimento circular
+        // nave.model.position.x = Math.sin(Date.now() * 0.001) * 5;
+        // nave.model.position.z = Math.cos(Date.now() * 0.001) * 5;
+    }
+
 
 	renderer.setAnimationLoop(animate);
 }
