c8d4754e74e0621b031d1fd467a53d397829b652
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2024-09-26 14:14:31 -0300

    Buraco negro

--- Galaxy Destroy/main.js
+++ Galaxy Destroy/main.js
@@ -17,66 +17,57 @@ class NaveEspacial {
 		this.load(this);
 	}
 
-	move() {
-		// this.model.position.z += 0.03; // Adjust the speed here
-		this.model.rotation.y += 0.02;
-
-		// if(this.model){
-		// 	this.model.position.z += 0.01;
-
-		// 	if(this.model.position.z >= 5){
-		// 		this.model.position.z = -20.0;
-		// 		let r = Math.random();
-		// 		this.model.position.x = -15.0 *  r + 15.0 * (1.0 - r);
-		// 		this.model.position.y = -15.0 *  r + 15.0 * (1.0 - r);
-		// 	}
-		// }
-	}
 	load(object) {
-
+		
 		const loader = new GLTFLoader();
-
+		
 		loader.load(
 			// resource URL
 			'models/Spaceship.gltf',
 			// called when the resource is loaded
 			function (gltf) {
-
-
+				
+				
 				scene.add(gltf.scene);
 				object.model = gltf.scene.children[0];
-
+				
 				// model = glb.scene.children[0];
 				// console.log(model);
 				// scene.add( model );
 				// model = glb.scene.children[12];
 				// this.scene.add = (this.model)
-
+				
 				// scene.add( gltf.scene );
-
+				
 				gltf.animations; // Array<THREE.AnimationClip>
 				gltf.scene; // THREE.Group
 				gltf.scenes; // Array<THREE.Group>
 				gltf.cameras; // Array<THREE.Camera>
 				gltf.asset; // Object
-
+				
 			},
 			// called while loading is progressing
 			function (xhr) {
-
+				
 				console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
+				
 			},
 			// called when loading has errors
 			function (error) {
-
+				
 				console.log('An error happened');
 				console.log(error);
-
-
+				
+				
 			}
-
+			
 		);
+	}
+	move() {
+		if (this.model) {
+			this.model.position.z = 15;
+			this.model.scale.set(.1,.1,.1)
+		}
 	}
 }
 
@@ -86,74 +77,132 @@ class Sun {
 		this.light = null
 		this.load(this);
 	}
-
-	move() {
-		// this.model.position.z += 0.03; // Adjust the speed here
-		this.model.rotation.y += 0.02;
-
-		// if(this.model){
-		// 	this.model.position.z += 0.01;
-
-		// 	if(this.model.position.z >= 5){
-		// 		this.model.position.z = -20.0;
-		// 		let r = Math.random();
-		// 		this.model.position.x = -15.0 *  r + 15.0 * (1.0 - r);
-		// 		this.model.position.y = -15.0 *  r + 15.0 * (1.0 - r);
-		// 	}
-		// }
-	}
+	
 	load(object) {
-
+		
 		const loader = new GLTFLoader();
-
+		
 		loader.load(
 			// resource URL
 			'models/Sun.gltf',
 			// called when the resource is loaded
 			function (gltf) {
-
-
+				
+				
 				scene.add(gltf.scene);
 				object.model = gltf.scene.children[0];
-
+				
 				const light = new THREE.PointLight(0xff0000, 1, 100);
 				object.light = light;
 				object.model.add(light);
-
+				
 				// model = glb.scene.children[0];
 				// console.log(model);
 				// scene.add( model );
 				// model = glb.scene.children[12];
 				// this.scene.add = (this.model)
-
+				
 				// scene.add( gltf.scene );
-
+				
 				gltf.animations; // Array<THREE.AnimationClip>
 				gltf.scene; // THREE.Group
 				gltf.scenes; // Array<THREE.Group>
 				gltf.cameras; // Array<THREE.Camera>
 				gltf.asset; // Object
-
+				
 			},
 			// called while loading is progressing
 			function (xhr) {
-
+				
 				console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
+				
 			},
 			// called when loading has errors
 			function (error) {
-
+				
 				console.log('An error happened');
 				console.log(error);
-
-
+				
+				
 			}
-
+			
 		);
+	}
+	move() {
+		if (this.model) {
+			
+			this.model.rotation.y += 0.01; // Rotate the Sun (optional)
+			this.model.scale.set(5,5,5)
+			this.model.position.z = 0
+		}
+
 	}
 }
 
+class blackHole {
+	constructor() {
+		this.model = null;
+		this.load(this);
+	}
+	
+	load(object) {
+		
+		const loader = new GLTFLoader();
+		
+		loader.load(
+			// resource URL
+			'models/Black hole.gltf',
+			// called when the resource is loaded
+			function (gltf) {
+				
+				
+				scene.add(gltf.scene);
+				object.model = gltf.scene.children[0];
+				
+				
+				// model = glb.scene.children[0];
+				// console.log(model);
+				// scene.add( model );
+				// model = glb.scene.children[12];
+				// this.scene.add = (this.model)
+				
+				// scene.add( gltf.scene );
+				
+				gltf.animations; // Array<THREE.AnimationClip>
+				gltf.scene; // THREE.Group
+				gltf.scenes; // Array<THREE.Group>
+				gltf.cameras; // Array<THREE.Camera>
+				gltf.asset; // Object
+				
+			},
+			// called while loading is progressing
+			function (xhr) {
+				
+				console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+				
+			},
+			// called when loading has errors
+			function (error) {
+				
+				console.log('An error happened');
+				console.log(error);
+				
+				
+			}
+			
+		);
+	}
+	move() {
+		if (this.model) {
+			
+			this.model.rotation.y += 0.01; // Rotate the Sun (optional)
+			this.model.scale.set(5,5,5)
+			this.model.position.z = -400
+			this.model.position.y = -100
+		}
+
+	}
+}
 
 class Planet {
 	constructor(modelPath, orbitRadius, orbitSpeed) {
@@ -189,6 +238,7 @@ class Planet {
 
 	move() {
 		if (this.model) {
+			this.model.scale.set(3,3,3)
 			this.model.rotation.y += 0.02;
 	  
 			this.angle += this.orbitSpeed; // Update the planet's angle for orbiting
@@ -200,23 +250,24 @@ class Planet {
 	}
 }
 
-// let nave = new NaveEspacial();
+let nave = new NaveEspacial();
 let sol = new Sun();
 let venus = new Planet('models/Venus.gltf', 20, 0.01);
 let mercurio = new Planet('models/Mercury.gltf', 15, 0.02);
 let terra = new Planet('models/Earth and Moon.gltf', 22, 0.005);
 let marte = new Planet('models/Mars.gltf', 24, 0.006);
-let jupiter = new Planet('models/Jupiter.gltf', 29, 0.006);
-let saturno = new Planet('models/Saturn.gltf', 35, 0.002);
-let urano = new Planet('models/Uranus.gltf', 39, 0.004);
-let netuno = new Planet('models/Neptune.gltf', 43, 0.004);
+let jupiter = new Planet('models/Jupiter.gltf', 30, 0.006);
+let saturno = new Planet('models/Saturn.gltf', 39, 0.002);
+let urano = new Planet('models/Uranus.gltf', 30, 0.004);
+let netuno = new Planet('models/Neptune.gltf', 40, 0.004);
 let plutao = new Planet('models/Pluto.gltf', 10, 0.04);
+let buraco = new blackHole();
 
 let light = new THREE.AmbientLight(0xffffff);
 scene.add(light);
 
 // const light = new THREE.PointLight( 0xff0000, 1, 100 );
-// light.position.set( sol.model.position.x, sol.model.position.y , sol.model.position.z );
+// light.position.set( 0, 0 , -400 );
 // scene.add( light );
 
 const controls = new OrbitControls( camera, renderer.domElement );
@@ -229,6 +280,8 @@ camera.position.y = 5;
 function animate() {
 	renderer.render(scene, camera);
 
+	buraco.move();
+	nave.move();
 	venus.move();
 	mercurio.move();
 	terra.move();
@@ -238,12 +291,7 @@ function animate() {
 	urano.move();
 	netuno.move();
 	plutao.move();
-
-	 if (sol.model) {
-	 	sol.model.rotation.y += 0.01; // Rotate the Sun (optional)
-		sol.model.scale.set(5,5,5)
-		sol.model.position.z = 0
-	 }
+	sol.move();
 
 	 controls.update();
 
