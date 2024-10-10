de0443fe9cf22b6dd03a099f1934159a87817bd8
PollyanaAGW <pollyanadias91@gmail.com>
2024-10-10 15:14:25 -0300

    to colocando as funcoes mas sem testar

--- Galaxy Destroy/main.js
+++ Galaxy Destroy/main.js
@@ -450,6 +450,13 @@ function animate() {
 				break;
 			}
 
+			if (checkCollision(missiles[i], marte)) {
+				marte.vida -= missiles[i].dano;
+				missiles[i].destroy();
+				missiles.splice(i);
+				break;
+			}
+
 			else if (missiles[i].model.position.z <= -500) {
 				console.log('sai');
 				missiles[i].destroy();
